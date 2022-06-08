b93455dfb19d2d03ea9a96aca5c6221d357c8f85
marwahadhraoui <hadhraouimarwa21@gmail.com>
2022-06-08 20:25:07 +0200

    update in schema

--- greenproject.sql
+++ greenproject.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 08, 2022 at 12:56 AM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 8.0.9
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mer. 08 juin 2022 à 18:24
+-- Version du serveur : 5.7.36
+-- Version de PHP : 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,16 +18,17 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `greenproject`
+-- Base de données : `greenproject`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bin`
+-- Structure de la table `bin`
 --
 
-CREATE TABLE `bin` (
+DROP TABLE IF EXISTS `bin`;
+CREATE TABLE IF NOT EXISTS `bin` (
   `id` int(11) NOT NULL,
   `location` varchar(50) NOT NULL,
   `storage` int(11) NOT NULL
@@ -36,71 +37,79 @@ CREATE TABLE `bin` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `request`
+-- Structure de la table `request`
 --
 
-CREATE TABLE `request` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `request`;
+CREATE TABLE IF NOT EXISTS `request` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `req-title` varchar(50) NOT NULL,
   `note` varchar(255) NOT NULL,
-  `title` varchar(50) NOT NULL,
   `volume` int(50) NOT NULL,
   `categorie` varchar(50) NOT NULL,
-  `poids` int(11) NOT NULL,
   `location` varchar(50) NOT NULL,
-  `isValid` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `isValid` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `request`
+--
+
+INSERT INTO `request` (`id`, `req-title`, `note`, `volume`, `categorie`, `location`, `isValid`) VALUES
+(1, 'depot de dechets', 'je veux deposer les dechets', 50, 'dechets menagers', 'nabeul', 0),
+(2, 'depot de dechets', 'je veux deposer mes dechets chez vous', 70, 'papiers', 'tunis', 0),
+(3, 'depots dechets', 'je veux deposer les dechets', 80, 'piles', 'kef', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `transactions`
+-- Structure de la table `transactions`
 --
 
-CREATE TABLE `transactions` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `transactions`;
+CREATE TABLE IF NOT EXISTS `transactions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `driver_id` int(11) NOT NULL,
-  `waste_id` int(11) NOT NULL
+  `waste_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `transactions`
---
-
-INSERT INTO `transactions` (`id`, `user_id`, `driver_id`, `waste_id`) VALUES
-(1, 1, 2, 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Structure de la table `user`
 --
 
-CREATE TABLE `user` (
-  `immat` int(11) NOT NULL,
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `immat` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL,
-  `email` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
   `number` int(50) NOT NULL,
-  `role` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `role` varchar(20) NOT NULL,
+  PRIMARY KEY (`immat`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `user`
+-- Déchargement des données de la table `user`
 --
 
 INSERT INTO `user` (`immat`, `username`, `password`, `email`, `number`, `role`) VALUES
-(1, 'aladdin', '123', 'ala.salhaoui.re@gmail.com', 12345678, 'USER'),
-(2, 'driver', '123', 'drive@gmail.com', 12345678, 'DRIVER');
+(1, 'Marwa Hadhraoui', '123', 'hadhraouimarwa21@gmail.com', 29386718, 'USER'),
+(2, 'Marwaaaaa', '123456', 'hadhraouimarwa21@gmail.com', 29386718, 'CHAUFFEUR'),
+(5, 'ala', '123', 'ala@gmail.com', 12345678, 'GARDIEN');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `waste`
+-- Structure de la table `waste`
 --
 
-CREATE TABLE `waste` (
+DROP TABLE IF EXISTS `waste`;
+CREATE TABLE IF NOT EXISTS `waste` (
   `id` int(11) NOT NULL,
   `title` varchar(50) NOT NULL,
   `volume` int(11) NOT NULL,
@@ -108,57 +117,6 @@ CREATE TABLE `waste` (
   `poids` int(11) NOT NULL,
   `location` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `waste`
---
-
-INSERT INTO `waste` (`id`, `title`, `volume`, `categorie`, `poids`, `location`) VALUES
-(1, 'Garage waste', 200, 'Normal', 20, 'Nabeul');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `request`
---
-ALTER TABLE `request`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `transactions`
---
-ALTER TABLE `transactions`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`immat`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `request`
---
-ALTER TABLE `request`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `transactions`
---
-ALTER TABLE `transactions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `immat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
